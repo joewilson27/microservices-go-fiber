5b427472b4c007e4d17114e90709a9fe6268c4d2
Joe Wilson <wilsonmangunsong@gmail.com>
2023-07-30 01:09:47 +0700

    part-3
    - Configure Database
    - Setup Models
    - Migrations

---
+++ services/web_blog/database/database.go
@@ -0,0 +1,37 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/beego/beego/v2/client/orm"
+	_ "github.com/lib/pq"
+)
+
+type DBInstance struct {
+	Db *sql.DB
+}
+
+var Database DBInstance
+
+func ConnectDB() {
+	// gunakan 5432 (port di docker, karena akan jalan di image docker) --> 5435:5432
+	var dbUrl = "postgresql://postgres:postgres@db:5432/web_microservice?sslmode=disable"
+	orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	// "default" alias, "postgres" driver name, dbUrl datasource
+	if err := orm.RegisterDataBase("default", "postgres", dbUrl); err != nil {
+		log.Fatal(err.Error())
+	}
+
+	if db, err := sql.Open("postgres", dbUrl); err != nil {
+		log.Fatal(err.Error())
+	} else {
+		if err := orm.RunSyncdb("default", false, true); err != nil {
+			log.Fatal(err.Error())
+		} else {
+			Database = DBInstance{Db: db}
+			log.Print("Connected Succcessfully")
+		}
+	}
+}
--- services/web_blog/main.go
+++ services/web_blog/main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/beego/beego/v2/client/orm"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joewilson27/microservices-go-fiber/database"
+	"github.com/joewilson27/microservices-go-fiber/models"
 )
 
 func main() {
@@ -16,6 +19,14 @@ func main() {
 	app.Get("/hello", func(c *fiber.Ctx) error {
 		return c.SendString("Hello Mars!")
 	})
+
+	// register model to database, right before we call ConnectDB() function.
+	// jika ingin di register pada file database juga bisa, caranya register setelah baris code
+	// orm.RegisterDataBase
+	orm.RegisterModel(new(models.Authors))
+	// connect db
+	database.ConnectDB()
+
 	addr := fmt.Sprintf(":%d", port)
 	log.Fatal(app.Listen(addr))
 }
