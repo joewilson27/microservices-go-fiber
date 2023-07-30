8bb5d47b940d1c41af6fcb6fdeb13388c8fd9392
Joe Wilson <wilsonmangunsong@gmail.com>
2023-07-30 20:22:49 +0700

    part-4
    - Environment variable (struct response, model post)
    - validators
    - create handlers

--- services/web_blog/database/database.go
+++ services/web_blog/database/database.go
@@ -4,20 +4,28 @@ import (
 	"database/sql"
 	"log"
 
+	"fmt"
+	"os"
+
 	"github.com/beego/beego/v2/client/orm"
 	_ "github.com/lib/pq"
 )
 
 type DBInstance struct {
-	Db *sql.DB
+	Db  *sql.DB
+	Orm orm.Ormer
 }
 
 var Database DBInstance
 
 func ConnectDB() {
 	// gunakan 5432 (port di docker, karena akan jalan di image docker) --> 5435:5432
-	var dbUrl = "postgresql://postgres:postgres@db:5432/web_microservice?sslmode=disable"
-	orm.RegisterDriver("postgres", orm.DRPostgres)
+	// var dbUrl = "postgresql://postgres:postgres@db:5432/web_microservice?sslmode=disable"
+	// orm.RegisterDriver("postgres", orm.DRPostgres)
+
+	// substituted our env vars into our connection string.
+	dbUrl := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
 
 	// "default" alias, "postgres" driver name, dbUrl datasource
 	if err := orm.RegisterDataBase("default", "postgres", dbUrl); err != nil {
@@ -30,7 +38,10 @@ func ConnectDB() {
 		if err := orm.RunSyncdb("default", false, true); err != nil {
 			log.Fatal(err.Error())
 		} else {
-			Database = DBInstance{Db: db}
+			// We set the debug field to true so we can see the queries in our terminal.
+			orm.Debug = true
+			o := orm.NewOrm()
+			Database = DBInstance{Db: db, Orm: o}
 			log.Print("Connected Succcessfully")
 		}
 	}
